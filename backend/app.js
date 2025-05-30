b89e388c92850cc9fe7038d9c4e75a64d25572a7
satti-hari-krishna-reddy <hari.100.hks@outlook.com>
2025-05-30 20:26:16 +0530

    add cors

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const productRoutes = require('./routes/productRoutes');
 
 const app = express();
+
+// Allow all origins (development use)
+app.use(cors());
 app.use(express.json());
 
 // Routes
 app.use('/api', productRoutes);
 
-module.exports = app;   
+module.exports = app;
